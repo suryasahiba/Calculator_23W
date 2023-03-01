0e9d198e0df08e6937938375577a123077f82097
Jae Aeich <javedh@iitbhilai.ac.in>
2023-03-01 12:43:10 +0530

    fixed error and logical and

--- __init__.py
+++ __init__.py
@@ -29,7 +29,7 @@ def SUB():
     sub=a-b
     response = "Difference = " + str(sub)
     return response
-@app.route("/logicalAND", methods=["POST"])
+@app.route("/LOGICALAND", methods=["POST"])
 def LOGICALAND(): 
 
     jsonStr = request.get_json()
@@ -37,7 +37,8 @@ def LOGICALAND():
     
     a=int(jsonObj['N1'])
     b=int(jsonObj['N2'])
-    response = "Logical AND is = " + str(LOGICALAND)
+    ans= a and b
+    response = "Logical AND is = " + str(ans)
     return response
 
 @app.route("/exp", methods=["POST"])
