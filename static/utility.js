685ee111bfaa13c4ecf19229c8efc929aea5a90d
jainpalkesh12 <palkeshjain@iitbhilai.ac.in>
2023-03-01 13:03:27 +0530

    BitwiseAND function added

--- static/utility.js
+++ static/utility.js
@@ -54,3 +54,4 @@ function showOutput(output) {
 	division.innerHTML = output;
 }
 
+function bitwiseAND(){var a = parseINt(document.getElementById('N1').value);var b = parseInt(document.getElementById('N2').value);var result = N1&n2;document.getElementById('output').value = result;}
