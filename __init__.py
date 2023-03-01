f1f8c8839cb90a5471c26f5ecc283d574781d45c
suryasahiba <46808361+suryasahiba@users.noreply.github.com>
2023-03-01 11:58:44 +0530

    First Commit

---
+++ __init__.py
@@ -0,0 +1,33 @@
+import json
+from flask import Flask, render_template, request, jsonify   
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("InputOutput.html")        
+
+@app.route("/add", methods=["POST"])
+def ADD(): 
+    jsonStr = request.get_json()
+    jsonObj = json.loads(jsonStr)
+    
+    a=int(jsonObj['N1'])
+    b=int(jsonObj['N2'])
+    sum=a+b
+    response = "sum = " + str(sum)
+    return response
+
+@app.route("/sub", methods=["POST"])
+def SUB(): 
+    jsonStr = request.get_json()
+    jsonObj = json.loads(jsonStr)
+    
+    a=int(jsonObj['N1'])
+    b=int(jsonObj['N2'])
+    sub=a-b
+    response = "Difference = " + str(sub)
+    return response
+
+if __name__== "__main__":
+    app.run()
